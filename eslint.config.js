e2638c6d05fe17104e14eaf40574a7899c610757
nekorush14 <mitsuhiro.komuro@gmail.com>
2025-02-23 15:57:07 +0900

    feat(configs): Add initial ESLint and Prettier configuration files

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+// @ts-check
+import eslint from "@eslint/js";
+import angular from "angular-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...angular.configs.tsRecommended,
+      eslintPluginPrettierRecommended,
+      eslintConfigPrettier,
+    ],
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+      "prettier/prettier": [
+        "error",
+        {
+          semi: true,
+          indent: 2,
+          singleQuote: false,
+          trailingComma: "es5",
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  },
+);
